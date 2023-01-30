b15ebd34a47b3169d758decbe5a1800f5aa20d23
iswa-git <ibrikisw@gmail.com>
2023-01-31 01:23:50 +0200

    Initial commit

---
+++ server/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser1: process.env.PGUSER,
+  pgHost: process.env.PGHOST,
+  pgDatabase: process.env.PGDATABASE,
+  pgPassword1: process.env.PGPASSWORD,
+  pgPort: process.env.PGPORT,
+  pgPassword: process.env.POSTGRES_PASSWORD,
+  pgUser: process.env.POSTGRES_USER,
+};
